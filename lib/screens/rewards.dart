e5c20c84d26e682f1ca83a32d4f57cfdfe075072
Onur Erhan <contact@onurerhan.com>
2020-12-26 21:19:05 +0300

    Added dynamic achievement list

--- lib/screens/rewards.dart
+++ lib/screens/rewards.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:math';
 import 'package:fotojenico/navbar.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../globals.dart';
 
 class RewardsScreen extends StatefulWidget {
   @override
@@ -14,21 +21,77 @@ class RewardsScreen extends StatefulWidget {
 }
 
 class _RewardsState extends State<RewardsScreen> {
-  Widget rewardWidget(int totalSteps){
-    return CircularStepProgressIndicator(
-      totalSteps: totalSteps,
-      currentStep: 5,
-      stepSize: 10,
-      selectedColor: Theme.of(context).accentColor,
-      unselectedColor: Theme.of(context).backgroundColor,
-      padding: pi / 200,
-      width: 150,
-      height: 150,
-      child: Icon(
-        FontAwesomeIcons.smile,
-        color: Theme.of(context).primaryColor,
-        size: 84,
-      ),
+  bool loading = false;
+  bool start = true;
+  List data = [];
+  var achievements;
+
+  dynamic loadJson() async {
+    String data = await rootBundle.loadString('assets/achievements.json');
+    var jsonResult = json.decode(data);
+    print(jsonResult);
+    achievements = jsonResult;
+  }
+
+
+  Future<Null> getDataList() async {
+    setState(() {
+      loading = true;
+    });
+    String _token;
+    Future<IdTokenResult> idToken;
+    String achievementsUrl = webApiUrl + 'achievement_progress/';
+
+    await user.map((value) => idToken = value.user.getIdTokenResult(), empty: (_) {}, initializing: (_) {});
+    await idToken.then((value) => _token = value.token);
+    Map<String, String> header = new Map();
+    header["auth"] = _token;
+    try {
+      final request = await http.get(achievementsUrl, headers: header);
+      var response = json.decode(request.body);
+      setState(() {
+        if (start){
+          data.insertAll(0, response);
+          start = false;
+        }
+      });
+    } catch (e) {
+      print('caught generic exception');
+      print(e);
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+  void initState(){
+    super.initState();
+    if(!loading && start){
+      loadJson();
+      getDataList();
+    }
+  }
+
+  Widget rewardWidget(int index){
+    var widgetData = data[index];
+    String rewardLabel = achievements[widgetData['achievement'].toString()]['label'] ?? '';
+    return Column(
+      children: [
+        CircularStepProgressIndicator(
+          totalSteps: widgetData['step_count'] ?? 1,
+          currentStep: widgetData['progress_step'],
+          stepSize: 10,
+          selectedColor: Theme.of(context).accentColor,
+          unselectedColor: Theme.of(context).backgroundColor,
+          padding: pi / 200,
+          width: 150,
+          height: 150,
+          child: SvgPicture.asset(
+              'assets/images/' + achievements[widgetData['achievement'].toString()]['image'],
+              semanticsLabel: 'A red up arrow'
+          ),
+        ),
+        Text(rewardLabel),
+      ],
     );
   }
 
@@ -39,10 +102,10 @@ class _RewardsState extends State<RewardsScreen> {
           alignment: Alignment.center,
           child: StaggeredGridView.countBuilder(
             crossAxisCount: 4,
-            itemCount: 20,
+            itemCount: data.length,
             itemBuilder: (BuildContext context, int index) => new Container(
                 child: new Center(
-                  child: rewardWidget(15),
+                  child: rewardWidget(index),
                 )),
             staggeredTileBuilder: (int index) => new StaggeredTile.count(2, 2),
             mainAxisSpacing: 4.0,
